3b88580824c2c9824f2dba180186373025d42a98
Mike Keen <mwk@mikekeen.com>
2022-09-19 12:58:28 -0400

    Configurable S3 client

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 
 
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
 
-const listAllObjects = async (Bucket, Prefix = "", onlyKeys = true, ContinuationToken = null, results = []) => {
-  const res = await s3.listObjectsV2({Bucket, Prefix, ContinuationToken}).promise();
+const listAllObjects = async (S3Client, Bucket, Prefix = "", onlyKeys = true, ContinuationToken = null, results = []) => {
+  const res = await S3Client.listObjectsV2({Bucket, Prefix, ContinuationToken}).promise();
   const reducedRes = onlyKeys ? res.Contents.map( k => k.Key ) : res.Contents;
   results = [ ...results, ...reducedRes];
   if(!!res.IsTruncated){
-    return await listAllObjects( Bucket, Prefix, onlyKeys, res.NextContinuationToken, results);
+    return await listAllObjects( S3Client, Bucket, Prefix, onlyKeys, res.NextContinuationToken, results);
   }
   return results;
 }
