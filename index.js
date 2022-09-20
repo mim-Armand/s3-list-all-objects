a347f86ada78444d4d01ab2aa134c2b59d7ac564
Mike Keen <mwk@mikekeen.com>
2022-09-20 13:25:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 
 const listAllObjects = async (Bucket, Prefix = "", onlyKeys = true, S3Client = new AWS.S3(), ContinuationToken = null, results = []) => {
-  const res = await client.listObjectsV2({Bucket, Prefix, ContinuationToken}).promise();
+  const res = await S3Client.listObjectsV2({Bucket, Prefix, ContinuationToken}).promise();
   const reducedRes = onlyKeys ? res.Contents.map( k => k.Key ) : res.Contents;
   results = [ ...results, ...reducedRes];
   if(!!res.IsTruncated){
