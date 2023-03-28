53d1e654752b8b3b9f95a769a1a8db207e0e21b9
Mike Keen <mwk@mikekeen.com>
2023-03-27 22:27:37 -0400

    blindly updating this

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3"
 
-const listAllObjects = async (Bucket, Prefix = "", onlyKeys = true, S3Client = new S3Client(), ContinuationToken = null, results = []) => {
+export const listAllObjects = async (Bucket, Prefix = "", onlyKeys = true, S3Client = new S3Client(), ContinuationToken = null, results = []) => {
   const res = await S3Client.listObjectsV2({Bucket, Prefix, ContinuationToken}).promise();
   const reducedRes = onlyKeys ? res.Contents.map( k => k.Key ) : res.Contents;
   results = [ ...results, ...reducedRes];
@@ -10,4 +10,3 @@ const listAllObjects = async (Bucket, Prefix = "", onlyKeys = true, S3Client = n
   return results;
 }
 
-exports.listAllObjects = listAllObjects;
